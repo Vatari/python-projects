74e41f2bf8379069ac379b0cd5ecb1c4aa1884a7
vatari <majestic13@gmail.com>
2023-10-30 07:56:59 +0200

    suff

---
+++ stuff.py
@@ -0,0 +1,9 @@
+# list.sort(reverse=True)
+# array.pop()
+# list.insert(i, element)
+# number = my_list(2)
+# repetition = my_list.count(2)
+# my_list.reverse()
+# del list(x)
+# my_list.remove(1)
+# isalnum():
